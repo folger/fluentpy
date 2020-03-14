49ccc979d53ce8ffd934efa3bcb69051a315914b
folger <folger@originlab.com.cn>
2020-03-14 19:43:32 +0800

    pdfextract.py

--- pdfextract/pdfextract.py
+++ pdfextract/pdfextract.py
@@ -1,13 +1,14 @@
 import os
-from tkinter import Tk, filedialog as fd
+# from tkinter import Tk, filedialog as fd
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
-root = Tk()
-root.iconify()
-filename = fd.askopenfilename(initialdir = "C:/",
-                                        title = "选择PDF文件",
-                                        filetypes = (("PDF Files", "*.PDF"), ("All files", "*.*")))
-root.withdraw()
+# root = Tk()
+# root.iconify()
+# filename = fd.askopenfilename(initialdir = "C:/",
+                                        # title = "选择PDF文件",
+                                        # filetypes = (("PDF Files", "*.PDF"), ("All files", "*.*")))
+# root.withdraw()
+filename = input('PDF文件：')
 if filename:
     s = input('提取页码：')
     pages = []
@@ -17,11 +18,12 @@ if filename:
             pages += list(range(int(rr[0]), int(rr[1])+1))
         else:
             pages.append(int(p))
-    fname, _ = os.path.splitext(os.path.basename(filename))
     pdf = PdfFileReader(filename)
     pdf_writer = PdfFileWriter()
     for page in pages:
         pdf_writer.addPage(pdf.getPage(page - 1))
-    output_filename = f'{fname}_extracted.pdf'
-    with open(output_filename, 'wb') as out:
+    path, name = os.path.split(filename)
+    fname, _ = os.path.splitext(name)
+    with open(os.path.join(path, f'{fname}_extracted.pdf'), 'wb') as out:
         pdf_writer.write(out)
+    os.system('pause')
