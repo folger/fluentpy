3d7066984f7411a186ec82f6abd69b333eb45ce9
folger <folger@originlab.com.cn>
2020-03-15 09:17:48 +0800

    pdfextract

--- pdfextract/pdfextract.py
+++ pdfextract/pdfextract.py
@@ -1,14 +1,7 @@
 import os
-# from tkinter import Tk, filedialog as fd
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
-# root = Tk()
-# root.iconify()
-# filename = fd.askopenfilename(initialdir = "C:/",
-                                        # title = "选择PDF文件",
-                                        # filetypes = (("PDF Files", "*.PDF"), ("All files", "*.*")))
-# root.withdraw()
-filename = input('PDF文件：')
+filename = input('PDF文件，可拖拽：')
 if filename:
     s = input('提取页码：')
     pages = []
@@ -23,7 +16,7 @@ if filename:
     for page in pages:
         pdf_writer.addPage(pdf.getPage(page - 1))
     path, name = os.path.split(filename)
-    fname, _ = os.path.splitext(name)
+    fname = os.path.splitext(name)[0]
     with open(os.path.join(path, f'{fname}_extracted.pdf'), 'wb') as out:
         pdf_writer.write(out)
     os.system('pause')
