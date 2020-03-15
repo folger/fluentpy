edc9b26ed42bbd2afbc7a2f530fe376d7421eee1
folger <folger@originlab.com.cn>
2020-03-15 09:17:37 +0800

    excel2pdf

--- excel2pdf/excel2pdf.py
+++ excel2pdf/excel2pdf.py
@@ -4,21 +4,20 @@ from win32com import client
 def exceltopdf(doc):
     if not doc:
         return
-    excel = client.DispatchEx("Excel.Application")
+    excel = client.DispatchEx('Excel.Application')
     excel.Visible = 0
     wb = excel.Workbooks.Open(doc)
     ws = wb.Worksheets[0]
 
     try:
-        fname, _ = os.path.splitext(os.path.basename(doc))
+        fname = os.path.splitext(os.path.basename(doc))[0]
         wb.SaveAs(fname, FileFormat=57)
     except Exception as e:
-        print("Failed to convert")
-        print(str(e))
+        print(f'Failed to convert: {e}')
     finally:
         wb.Close()
         excel.Quit()
         os.system('pause')
 
 if __name__ == '__main__':
-    exceltopdf(input('Excel文件：'))
+    exceltopdf(input('Excel文件，可拖拽：'))
